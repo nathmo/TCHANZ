19cb04589fbd7a491be4920fe96c177259ffec80
felipe <felipe1612@icloud.com>
2022-02-27 21:02:48 +0100

    test hj

---
+++ test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by felip on 27.02.2022.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "salut" << endl;
+    return 0;
+
+}
+
---
+++ test.h
@@ -0,0 +1,11 @@
+//
+// Created by felip on 27.02.2022.
+//
+
+
+
+
+#ifndef TCHANZ_TEST_H
+#define TCHANZ_TEST_H
+
+#endif //TCHANZ_TEST_H
