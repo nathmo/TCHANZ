fffa2e7d92cbe5de21ae5bd19a8f99adc94297a1
felipe <felipe1612@icloud.com>
2022-03-07 20:47:09 +0100

    added test for read function

--- testsuit.cc
+++ testsuit.cc
@@ -7,13 +7,25 @@
 
 #include <iostream>
 #include "testsuit.h"
+#include "textstorage.h"
 
 using namespace std;
 
 int main (void)
 {
+    assert_textstorage_readtxt();
     // add all test here (unit and integration + test where we read the scenario
     // file and play them
 
     return 0; // optionnel
+}
+
+bool assert_textstorage_readtxt()
+{
+    vector <string> testreadtxt = readtxt("scenario/no_error_collector_move.txt");
+    for (auto testreadtxt:stringtxt)
+    {
+        cout << stringtxt << endl;
+    }
+    return true;
 }
