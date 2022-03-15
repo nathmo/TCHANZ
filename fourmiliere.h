498dc2194b0fd348b85724a601840ceb41dc98c4
Nathann Morand <natmo@hotmail.ch>
2022-03-15 16:40:58 +0100

    Created class for fourmi, fourmiliere and nourriture

--- fourmiliere.h
+++ fourmiliere.h
@@ -8,10 +8,22 @@
 #ifndef TCHANZ_FOURMILIERE_H
 #define TCHANZ_FOURMILIERE_H
 
+#import "fourmi.h"
 
 namespace fourmiliere
 {
-
+    class Fourmiliere : public Entity
+    {
+    private:
+        int foodReserve;
+        int nbC;
+        int nbD;
+        int nbP;
+        vector<fourmi::Fourmi *> memberAnts;
+    public:
+        Fourmiliere(Point position, Point size, int total_food, int nbC, int nbD, int nbP );
+        update();
+    };
 }
 
 #endif //TCHANZ_FOURMILIERE_H
